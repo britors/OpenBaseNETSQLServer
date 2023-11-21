0beb1faa4e14004667b562dbc74e3150a08c8d2f
Rodrigo Brito <rodrigo@w3ti.com.br>
2023-11-20 21:10:11 -0300

    refact: refatorar controllers

---
+++ src/OpenBaseNET.Presentation.Api/Controllers/Customer/CreateCustomerController.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using OpenBaseNET.Application.DTOs.Customer.Requests;
+using OpenBaseNET.Application.Interfaces.Services;
+
+namespace OpenBaseNET.Presentation.Api.Controllers.Customer;
+
+[Route("api/customer")]
+[ApiController]
+public class CreateCustomerController(ICustomerApplicationService customerApplicationService): ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(
+        [FromBody] CreateCustomerRequest request,
+        CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var result
+                = await customerApplicationService.CreateAsync(request, cancellationToken);
+            return Ok(result);
+        }
+        catch (ValidationException ex)
+        {
+            return BadRequest(ex.Errors);
+        }
+        catch (OperationCanceledException)
+        {
+            return StatusCode(499);
+        }
+        catch (Exception ex)
+        {
+            return Problem(ex.Message);
+        }
+    }
+}
---
+++ src/OpenBaseNET.Presentation.Api/Controllers/Customer/FindCustomerByIdController.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using OpenBaseNET.Application.DTOs.Customer.Requests;
+using OpenBaseNET.Application.Interfaces.Services;
+
+namespace OpenBaseNET.Presentation.Api.Controllers.Customer;
+
+[Route("api/customer")]
+[ApiController]
+public class FindCustomerByIdController(ICustomerApplicationService customerApplicationService): ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetByIdAsync(
+        [FromQuery] FindCustomerByIdRequest request,
+        CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var result = await customerApplicationService.GetByIdAsync(request, cancellationToken);
+            return Ok(result);
+        }
+        catch (ValidationException ex)
+        {
+            return BadRequest(ex.Errors);
+        }
+        catch (OperationCanceledException)
+        {
+            return StatusCode(499);
+        }
+        catch (Exception ex)
+        {
+            return Problem(ex.Message);
+        }
+    }  
+}
