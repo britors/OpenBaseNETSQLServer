987a3c777b183b7fa25cefbf43d03a64979fc334
Rodrigo Brito <rodrigo@w3ti.com.br>
2023-11-26 13:21:41 -0300

    feat: implementar logs

--- src/OpenBaseNET.Infra.Data/Repositories/CustomerRepository.cs
+++ src/OpenBaseNET.Infra.Data/Repositories/CustomerRepository.cs
@@ -1,4 +1,5 @@
-﻿using OpenBaseNET.Domain.Entities;
+﻿using Microsoft.Extensions.Logging;
+using OpenBaseNET.Domain.Entities;
 using OpenBaseNET.Domain.Interfaces.Repositories;
 using OpenBaseNET.Infra.Data.Context;
 
@@ -7,8 +8,9 @@ namespace OpenBaseNET.Infra.Data.Repositories;
 public sealed class CustomerRepository : RepositoryBase<Customer>, ICustomerRepository, IDataRepository
 {
     public CustomerRepository(DbSession dbSession,
+       ILogger<RepositoryBase<Customer>> logger,
         OneBaseDataBaseContext context) :
-        base(dbSession, context)
+        base(dbSession, logger, context)
     {
     }
 }
--- src/OpenBaseNET.Infra.Data/Repositories/RepositoryBase.cs
+++ src/OpenBaseNET.Infra.Data/Repositories/RepositoryBase.cs
@@ -4,16 +4,22 @@ using OpenBaseNET.Infra.EF.Extension;
 using System.Data;
 using System.Linq.Expressions;
 using OpenBaseNET.Infra.Data.Dapper.Extension;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
 
 namespace OpenBaseNET.Infra.Data.Repositories;
 
-public abstract class RepositoryBase<TEntity>(DbSession dbSession,
+public abstract class RepositoryBase<TEntity>
+    (
+        DbSession dbSession,
+        ILogger<RepositoryBase<TEntity>> logger,
         OneBaseDataBaseContext dbContext)
     : IRepositoryBase<TEntity>
     where TEntity : class
 {
     public async Task<TEntity> AddAsync(TEntity obj, CancellationToken cancellationToken)
     {
+        logger.LogInformation($"Adicionando {typeof(TEntity).Name} com valores {JsonSerializer.Serialize(obj)}");
         dbContext.Set<TEntity>().Add(obj);
         await dbContext.SaveChangesAsyncWtithRetry(cancellationToken);
         return obj;
@@ -24,25 +30,37 @@ public abstract class RepositoryBase<TEntity>(DbSession dbSession,
             Expression<Func<TEntity, bool>>? predicate = null,
             int? pageNumber = null,
             int? pageSize = null,
-            params Expression<Func<TEntity, object>>[] includes) =>
-        await dbContext.FindAsyncWithRetry(
+            params Expression<Func<TEntity, object>>[] includes)
+    {
+        logger.LogInformation($"Buscando por {typeof(TEntity).Name} com os seguintes filtros: {JsonSerializer.Serialize(predicate)} e includes {JsonSerializer.Serialize(includes)}");
+        var result = await dbContext.FindAsyncWithRetry(
             cancellationToken,
             predicate,
             pageNumber,
             pageSize,
             includes);
+        logger.LogInformation($"Resultado da busca por {typeof(TEntity).Name} com os seguintes filtros: {JsonSerializer.Serialize(predicate)} e includes {JsonSerializer.Serialize(includes)} foi {JsonSerializer.Serialize(result)}");
+        return result;
+    }
 
-    public async Task<TEntity?> GetByIdAsync<TKey>(TKey id, CancellationToken cancellationToken) 
-        => await dbContext.GetByIdAsyncWithRetry<TEntity, TKey>(id, cancellationToken);
+    public async Task<TEntity?> GetByIdAsync<TKey>(TKey id, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"Buscando por {typeof(TEntity).Name} com id {id}");
+        var result = await dbContext.GetByIdAsyncWithRetry<TEntity, TKey>(id, cancellationToken);
+        logger.LogInformation($"Resultado da busca por {typeof(TEntity).Name} com id {id} foi {result}");
+        return result;
+    }
 
     public async Task<bool> RemoveAsync(TEntity obj, CancellationToken cancellationToken)
     {
+        logger.LogInformation($"Removendo {typeof(TEntity).Name} com valores {JsonSerializer.Serialize(obj)}");
         dbContext.Set<TEntity>().Remove(obj);
         return await dbContext.SaveChangesAsyncWtithRetry(cancellationToken) > 0;
     }
 
     public async Task<bool> RemoveByIdAsync<TKey>(TKey id, CancellationToken cancellationToken)
     {
+        logger.LogInformation($"Removendo {typeof(TEntity).Name} com id {id}");
         var entity = await GetByIdAsync(id, cancellationToken);
         if (entity is null) return false;
         dbContext.Set<TEntity>().Remove(entity);
@@ -51,6 +69,7 @@ public abstract class RepositoryBase<TEntity>(DbSession dbSession,
 
     public async Task<TEntity> UpdateAsync(TEntity obj, CancellationToken cancellationToken)
     {
+        logger.LogInformation($"Atualizando {typeof(TEntity).Name} com valores {JsonSerializer.Serialize(obj)}");
         dbContext.Set<TEntity>().Update(obj);
         await dbContext.SaveChangesAsyncWtithRetry(cancellationToken);
         return obj;
@@ -59,12 +78,15 @@ public abstract class RepositoryBase<TEntity>(DbSession dbSession,
     public async Task<int> ExecuteAsync(string sql, CancellationToken cancellationToken, object? param = null)
     {
         if (dbSession.Connection is null) throw new ArgumentException(nameof(dbSession.Connection));
-        return await dbSession.Connection.ExecuteAsyncWithRetry(
+        logger.LogInformation($"Executando comando {sql}");
+        var result = await dbSession.Connection.ExecuteAsyncWithRetry(
             cancellationToken,
             sql,
             parameters: param,
             commandType: CommandType.Text,
             transaction: dbSession.Transaction);
+        logger.LogInformation($"Resultado da execução do comando {sql} foi {result}");
+        return result;
     }
 
     public async Task<IEnumerable<TResult>?> QueryAsync<TResult>(string query, CancellationToken cancellationToken,
@@ -72,12 +94,16 @@ public abstract class RepositoryBase<TEntity>(DbSession dbSession,
         where TResult : IEntityOrQueryResult
     {
         if (dbSession.Connection is null) throw new ArgumentException(nameof(dbSession.Connection));
-        return await dbSession.Connection.QueryAsyncWithRetry<TResult>(
+        logger.LogInformation($"Executando query {query}");
+        var result = await dbSession.Connection.QueryAsyncWithRetry<TResult>(
             cancellationToken,
             query,
             parameters: param,
             commandType: CommandType.Text,
             transaction: dbSession.Transaction);
+
+        logger.LogInformation($"Resultado da execução da query {query} foi {JsonSerializer.Serialize(result)}");
+        return result;
     }
 
     public async Task<TResult?> QueryFirstOrDefaultAsync<TResult>(string query, CancellationToken cancellationToken,
@@ -85,12 +111,15 @@ public abstract class RepositoryBase<TEntity>(DbSession dbSession,
         where TResult : IEntityOrQueryResult
     {
         if (dbSession.Connection is null) throw new ArgumentException(nameof(dbSession.Connection));
-        return await dbSession.Connection.QueryFirstOrDefaultAsyncWithRetry<TResult?>(
+        logger.LogInformation($"Executando query {query}");
+        var result = await dbSession.Connection.QueryFirstOrDefaultAsyncWithRetry<TResult?>(
             cancellationToken,
             query,
             parameters: param,
             commandType: CommandType.Text,
             transaction: dbSession.Transaction);
+        logger.LogInformation($"Resultado da execução da query {query} foi {JsonSerializer.Serialize(result)}");
+        return result;
     }
 
     public async Task<TResult?> QuerySingleOrDefaultAsync<TResult>(string query,
@@ -99,15 +128,18 @@ public abstract class RepositoryBase<TEntity>(DbSession dbSession,
         where TResult : IEntityOrQueryResult
     {
         if (dbSession.Connection is null) throw new ArgumentException(nameof(dbSession.Connection));
-        return await dbSession.Connection.QuerySingleOrDefaultAsyncWithRetry<TResult?>(
+        logger.LogInformation($"Executando query {query}");
+        var result = await dbSession.Connection.QuerySingleOrDefaultAsyncWithRetry<TResult?>(
             cancellationToken,
             query,
             parameters: param,
             commandType: CommandType.Text,
             transaction: dbSession.Transaction);
+        logger.LogInformation($"Resultado da execução da query {query} foi {JsonSerializer.Serialize(result)}");
+        return result;
     }
 
-    public Task<int> CountAsync(CancellationToken cancellationToken, 
-        Expression<Func<TEntity, bool>>? predicate = null) 
+    public Task<int> CountAsync(CancellationToken cancellationToken,
+        Expression<Func<TEntity, bool>>? predicate = null)
         => dbContext.CountAsyncWithRetry(cancellationToken, predicate);
 }
