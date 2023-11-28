e14a05fa6e89f3890a3b524e3d9b9357d38d3417
Rodrigo Brito <rodrigo@w3ti.com.br>
2023-11-27 22:06:50 -0300

    feat: adicionar log no middleware

--- src/OpenBaseNET.Presentation.Api/ControllerMiddleware.cs
+++ src/OpenBaseNET.Presentation.Api/ControllerMiddleware.cs
@@ -1,9 +1,12 @@
 ﻿namespace OpenBaseNET.Presentation.Api;
 
-public class ControllerMiddleware(RequestDelegate next)
+public class ControllerMiddleware(RequestDelegate next, ILogger<ControllerMiddleware> logger)
 {
     public async Task InvokeAsync(HttpContext context)
     {
+        logger.LogInformation(context.Request.Path);
+        logger.LogInformation(context.Request.Method);
+        logger.LogInformation(context.Request.QueryString.Value);
         await next(context);
     }
 }
