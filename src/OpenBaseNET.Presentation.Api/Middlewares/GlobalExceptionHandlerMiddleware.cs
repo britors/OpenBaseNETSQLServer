a6d352f749e2ea4c5d2bc7cac4d04ce61aecaedb
Rodrigo Brito <rodrigo@w3ti.com.br>
2025-08-02 17:42:00 -0300

    fix: usar construtor primario

--- src/OpenBaseNET.Presentation.Api/Middlewares/GlobalExceptionHandlerMiddleware.cs
+++ src/OpenBaseNET.Presentation.Api/Middlewares/GlobalExceptionHandlerMiddleware.cs
@@ -2,16 +2,10 @@
 using System.Text.Json;
 
 namespace OpenBaseNET.Presentation.Api.Middlewares;
-public class GlobalExceptionHandlerMiddleware
+public class GlobalExceptionHandlerMiddleware(RequestDelegate next, ILogger<GlobalExceptionHandlerMiddleware> logger)
 {
-    private readonly RequestDelegate _next;
-    private readonly ILogger<GlobalExceptionHandlerMiddleware> _logger;
-
-    public GlobalExceptionHandlerMiddleware(RequestDelegate next, ILogger<GlobalExceptionHandlerMiddleware> logger)
-    {
-        _next = next;
-        _logger = logger;
-    }
+    private readonly RequestDelegate _next = next;
+    private readonly ILogger<GlobalExceptionHandlerMiddleware> _logger = logger;
 
     public async Task InvokeAsync(HttpContext context)
     {
