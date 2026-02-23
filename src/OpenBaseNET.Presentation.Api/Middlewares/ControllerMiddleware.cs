92fe01b56013559ab7f9ec8a4266c2c76dacfd0f
Rodrigo Brito <rodrigo@w3ti.com>
2026-02-23 11:18:13 -0300

    Feat: Imprementar logs  de informação no middleware

--- src/OpenBaseNET.Presentation.Api/Middlewares/ControllerMiddleware.cs
+++ src/OpenBaseNET.Presentation.Api/Middlewares/ControllerMiddleware.cs
@@ -1,9 +1,17 @@
 namespace OpenBaseNET.Presentation.Api.Middlewares;
 
-public class ControllerMiddleware(RequestDelegate next)
+public class ControllerMiddleware(RequestDelegate next, ILogger<GlobalExceptionHandlerMiddleware> logger)
 {
   public async Task InvokeAsync(HttpContext context)
   {
+    
+    if(logger.IsEnabled(LogLevel.Information))
+    {
+      logger.LogInformation("ControllerMiddleware: Processando a requisição {Method} {Path}",
+      context.Request.Method,
+      context.Request.Path);
+    }
+
     await next(context);
   }
 }
