61c2aaf3954160b561b1487706f05d68356a2649
Rodrigo Brito <rodrigo@w3ti.com.br>
2026-03-10 20:34:09 -0300

    Chore: Atualizando pacotes nuget

--- src/OpenBaseNET.Presentation.Api/Middlewares/ControllerMiddleware.cs
+++ src/OpenBaseNET.Presentation.Api/Middlewares/ControllerMiddleware.cs
@@ -1,10 +1,9 @@
 namespace OpenBaseNET.Presentation.Api.Middlewares;
 
-public class ControllerMiddleware(RequestDelegate next, ILogger<GlobalExceptionHandlerMiddleware> logger)
+public class ControllerMiddleware(RequestDelegate next)
 {
   public async Task InvokeAsync(HttpContext context)
   {
     await next(context);
   }
 }
-
